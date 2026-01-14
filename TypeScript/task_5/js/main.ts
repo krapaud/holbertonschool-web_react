38b00d8f0ca466212c9ea3141052cb87fcf8a3b4
Krapaud <mur.mickael@gmail.com>
2026-01-14 15:35:09 +0100

    Implement Major and Minor credits interfaces with summation functions; update TypeScript configuration for module resolution

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -1,1 +1,23 @@
-// Task 5: Brand convention & Nominal typing
+interface MajorCredits {
+    credits: number;
+    __brand: 'MajorCredits';
+}
+
+interface MinorCredits {
+    credits: number;
+    __brand: 'MinorCredits';
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        __brand: 'MajorCredits'
+    };
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        __brand: 'MinorCredits'
+    };
+}
